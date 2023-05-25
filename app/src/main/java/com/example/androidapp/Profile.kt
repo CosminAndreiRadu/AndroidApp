050f9bd7a174b1dcd83c4d4553e87a6689ae1fed
Cosmin <cosminradu1760@yahoo.com>
2023-05-25 21:10:55 +0300

    added signin/signup with firebase

--- app/src/main/java/com/example/androidapp/Profile.kt
+++ app/src/main/java/com/example/androidapp/Profile.kt
@@ -3,16 +3,30 @@ package com.example.androidapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.example.androidapp.databinding.ActivityMainBinding
 import com.example.androidapp.databinding.ActivityProfileBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class Profile : AppCompatActivity() {
     private lateinit var binding: ActivityProfileBinding
+    lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+        binding.logOutButton.setOnClickListener{
+            auth.signOut()
+            startActivity(Intent(this, SignInActivity::class.java))
+            finish()
+        }
+
+        val txt = auth.currentUser?.email.toString()
+        val myTextView = findViewById<TextView>(R.id.textView2)
+        myTextView.text = "Connected as: " + txt
 
         binding.bottomNavigationView.setOnNavigationItemSelectedListener {
 
